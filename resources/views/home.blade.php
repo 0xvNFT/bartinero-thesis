79d53ee166fd4b9f6b572e45fce878652b23a543
Francois Saunar <fdcsaunar@gmail.com>
2021-01-03 00:45:33 +0800

    main

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,13 +3,23 @@
 @section('content')
 
 <section id="hero">
-    <div class="hero-slider">
+
+    <div class="container">
+        <h1>The barter hub for the<br>Las Piñas community</h1>
+        <div class="hero-cta">
+            <a href="#short-info">Why barter?</a>
+            <a href="/register">Start trading now</a>
+        </div>
+    </div>
+
+
+    {{-- <div class="hero-slider">
         {{-- <div><img src="{{ asset('img/cat-banner-sample.png') }}" alt=""></div> --}}
-        <div style="background-color: blue">Test</div>
+        {{-- <div style="background-color: blue">Test</div>
         <div style="background-color: red">Test</div>
         <div style="background-color: yellow">Test</div>
         <div style="background-color: green">Test</div>
-    </div>
+    </div> --}}
 </section>
 
 <section id="prodcat-slider">
@@ -21,7 +31,7 @@
             <div class="cat-item">
                 <i class="fas fa-utensils"></i>
                 <h3>Food</h3>
-            </div>
+            </div> 
 
             <div class="cat-item">
                 <i class="fas fa-mobile"></i>
