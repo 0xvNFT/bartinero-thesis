88f537099f0bec564231a183fe31fc9f7bc98586
Francois Saunar <fdcsaunar@gmail.com>
2021-01-04 06:16:58 +0800

    main

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -10,7 +10,7 @@
             <div class="col-lg-5">
                 <h1>A hub for the<br>Las Piñas barter<br>community.</h1>
                 <div class="hero-cta">
-                    <a href="#short-info">Why barter?</a>
+                    <a href="#about">Why barter?</a>
                     <a href="/register">Start trading now</a>
                 </div>
             </div>
@@ -42,7 +42,7 @@
                 </div>
 
                 <div class="byline-text">
-                    <h1>Post a deal</h1>
+                    <h1>Post a deal.</h1>
                     <p>Barter old or new items. Even services.</p>
                 </div>
                     
@@ -55,7 +55,7 @@
                 </div>
 
                 <div class="byline-text">
-                    <h1>Choose an offer</h1>
+                    <h1>Choose an offer.</h1>
                     <p>Plenty of options to choose from.</p>
                 </div> 
 
@@ -68,7 +68,7 @@
                 </div>
                 
                 <div class="byline-text">
-                    <h1>Trade with ease</h1>
+                    <h1>Trade with ease.</h1>
                     <p>Communicate within the site.</p>
                 </div>
                 
@@ -84,45 +84,61 @@
         <p>Choose among these categories for new or used items</p>
         <div class="cat-selector">
 
-            <div class="cat-item">
-                <i class="fas fa-utensils"></i>
-                <h3>Food</h3>
-            </div> 
+            <a href="/">
+                <div class="cat-item">
+                    <i class="fas fa-utensils"></i>
+                    <h3>Food</h3>
+                </div> 
+            </a>
+            
+            <a href="/">
+                <div class="cat-item">
+                    <i class="fas fa-mobile"></i>
+                    <h3>Electronics</h3>
+                </div>
+            </a>
 
-            <div class="cat-item">
-                <i class="fas fa-mobile"></i>
-                <h3>Electronics</h3>
-            </div>
+            <a href="/">
+                <div class="cat-item">
+                    <i class="fas fa-leaf"></i>
+                    <h3>Gardening</h3>
+                </div>
+            </a>
+            
+            <a href="/">
+                <div class="cat-item">
+                    <i class="fas fa-tshirt"></i>
+                    <h3>Clothing</h3>
+                </div>
+            </a>
 
-            <div class="cat-item">
-                <i class="fas fa-leaf"></i>
-                <h3>Gardening</h3>
-            </div>
+            <a href="/">
+                <div class="cat-item">
+                    <i class="fas fa-tools"></i>
+                    <h3>Services</h3>
+                </div>
+            </a>
 
-            <div class="cat-item">
-                <i class="fas fa-tshirt"></i>
-                <h3>Clothing</h3>
-            </div>
+            <a href="/">
+                <div class="cat-item">
+                    <i class="fas fa-car"></i>
+                    <h3>Automotives</h3>
+                </div>
+            </a>
 
-            <div class="cat-item">
-                <i class="fas fa-tools"></i>
-                <h3>Services</h3>
-            </div>
+            <a href="/">
+                <div class="cat-item">
+                    <i class="fas fa-heart"></i>
+                    <h3>Health & Wellness</h3>
+                </div>
+            </a>
 
-            <div class="cat-item">
-                <i class="fas fa-car"></i>
-                <h3>Automotives</h3>
-            </div>
-
-            <div class="cat-item">
-                <i class="fas fa-heart"></i>
-                <h3>Health & Wellness</h3>
-            </div>
-
-            <div class="cat-item">
-                <i class="fas fa-couch"></i>
-                <h3>Furniture</h3>
-            </div>
+            <a href="/">
+                <div class="cat-item">
+                    <i class="fas fa-couch"></i>
+                    <h3>Furniture</h3>
+                </div>
+            </a>
 
         </div>
     </div>
@@ -132,25 +148,58 @@
     <div class="container">
         <h1>Check out these featured deals</h1>
         <p>Start trading with a Las Pinero</p>
+
+        <div class="featured-deals align-items-center justify-content-center d-flex">
+            <h1>Items will be posted here. At least 8.</h1>
+        </div>
+
     </div>
+
+
+
 </section>
  
 
-<section id="short-info">
+<section id="about">
     <div class="container">
 
         <h1>Why barter?</h1>
 
-        <div class="card">
-            <div class="card-body">
-                <h1 class="card-title">
-                    <i class="fas fa-recycle"></i>
-                    <br>
-                    Zero-waste lifestyle.
-                </h1>
+        <div class="row">
+
+            <div class="col-lg-4">
+                <div class="card">
+                    <div class="card-body">
+                        <h1 class="card-title">No money involved.</h1>
+                        <p>You don't have to shell out actual money to obtain an item. Just trade with whatever you have, whether it may be an item or a service.</p>
+                    </div>
+                </div>
             </div>
+
+            <div class="col-lg-4">
+                <div class="card">
+                    <div class="card-body">
+                        <h1 class="card-title">Environment-friendly.</h1>
+                        <p>The act of bartering also aims for a zero-waste lifestyle. Not only you can save money, but also the environment.</p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-lg-4">
+                <div class="card">
+                    <div class="card-body">
+                        <h1 class="card-title">Pragmatic solutions.</h1>
+                        <p>Bartering could act as a pragmatic solution for those who are in need. Especially for people who are affected badly by the pandemic.</p>
+                    </div>
+                </div>
+            </div>
+
         </div>
 
+        <div class="about-cta d-flex align-items-center justify-content-center">
+            <a href="/register">Join the Las Piñas barter community</a>
+        </div>
+    
     </div>
 </section>
 
